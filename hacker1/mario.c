0ff424fc83c3c9e578857b1825e75bd9c5eef2b0
Zach Toolson <zach.toolson@gmail.com>
2014-05-08 12:08:02 -0400

    commented mario.c hacker edition

--- hacker1/mario.c
+++ hacker1/mario.c
@@ -2,35 +2,35 @@
 #include <stdio.h>
 
 int main(void) {
-  // Ask user the height of the half pyramids
-  int height;
-  int i;
-  int j;
-  int index_range;
-
+  int height, i, j, index_range;
+  
+  /* Get user input */
   do
   {
     printf("Enter a height between 0 and 23: ");
     height = GetInt(); // GetInt ensures that an int was entered
   }
   while( height < 0 || height > 23);
-
+  
+  /* Calculate how large of an array is needed to store the pyramid */
   index_range  = height * 2 + 2;
 
+  /* Initialize and set all char to ' ' in the pyramid "string" */
   char pyramid[index_range];
   for(i = 0; i < index_range; ++i) {
     pyramid[i] = ' ';
   }
 
-  
+  /* Calculate which indices need to have a '#' in the */  
   for(j = 0; j < height; j++) {
-    pyramid[height - 1 - j] = '#';
-    pyramid[index_range - height + j] = '#';
-
+    pyramid[height - 1 - j] = '#'; /* Set first half pyramid '#' location */
+    pyramid[index_range - height + j] = '#'; /* Set second pyramid '#' location */
+    
+    /* Print out the level of the pyramid most recently updated */
     for(i = 0; i < index_range - height + j + 1; ++i) {
       printf("%c", pyramid[i]);
     }
-
+  
     printf("\n");
   }
 
