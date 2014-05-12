c3618d0f22a34cd97a06fba2f62acaf77621df96
Zach Toolson <zach.toolson@gmail.com>
2014-05-12 12:24:05 -0400

    caesar cipher working. Need to fix command line checking

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -3,24 +3,55 @@
   Date: 5/12/2014
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(int argc, char * argv[]) {
   // Program accepts one command-line argument which is a non-negative number.
   // Will return 1 if this is not the case
 
+  //@TODO fix way that the program terminates with incorrect command line arguments
   if (argc != 2) {
     return 1;
   }
 
-  int input = atoi(argv[1]); // convert command line to int
-  if (input <= 0) {
+  // convert command line to int, this is the offset for caesar cipher
+  int k = atoi(argv[1]);
+  if (k <= 0) {
     return 1;
   }
-  
-  for (int i = 0; i < argc; ++i) {
-    printf("Argument %i is %s\n", i, argv[i]);
+
+  // Prompt user for a string of plain text
+  string user_text;
+  do {
+//    printf("Enter what you would like to be ciphered:\n");
+    user_text = GetString();
+  }
+  while( strcmp("", user_text) == 0 );
+
+  // Calculate caesar cipher of that string using k from command line
+  for (int i = 0, n = strlen(user_text); i < n; ++i) {
+    char sub_char;
+    bool conver_char = false; // do not convert punctuation
+    if (user_text[i] >= 97 && user_text[i] <= 122) {
+      conver_char = true;
+      sub_char = 'a';
+    }
+    else if ( user_text[i] >= 65 && user_text[i] <= 90) {
+      conver_char = true;
+      sub_char = 'A';
+    }
+
+    if (conver_char){
+      int temp = user_text[i] - sub_char; // Get number representation
+      temp = (temp + k) % 26; // Apply cipher
+      user_text[i] = temp + sub_char; // convert back to ascii
+    }
   }
 
-  printf("\nThe converted argv[1] is: %i\n", input);
+  // Output cipher text to user
+  printf("%s\n", user_text);
+  
+  return 0;
 }
